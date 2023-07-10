1e31e54974557a1ded6b6d1acb6f727f095795c5
Marjose <hypernate1@gmail.com>
2023-07-10 13:43:33 +0800

    added permission validation

--- config/backup-api.php
+++ config/backup-api.php
@@ -28,11 +28,12 @@ return [
      *  If the Permission set is blank, any authorized request will be granted for the action.
      *  -------
      *  You need to put permission needed by the user.
+     *  You need to supply it as a single permission string format
      */
     'permissions' => [
-        'download' => [],
-        'create_backup' => [],
-        'backup_list' => []
+        'download' => null,
+        'create_backup' => null,
+        'backup_list' => null
     ],
 
 ];
---
+++ src/Http/Controllers/BackupApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MarJose123\LaravelBackupApi\Http\Controllers;
+
+use F9Web\ApiResponseHelpers;
+use Illuminate\Http\Request;
+
+class BackupApiController
+{
+    use ApiResponseHelpers;
+
+    public function createBackup(Request $request)
+    {
+        /*
+         *  Check Authenticated User Permissions
+         */
+        if(!is_null(config('backup-api.permissions.create_backup')) && $request->user()->cannot(config('backup-api.permissions.create_backup')))
+        {
+            return $this->respondForbidden('You don\'t have a permission to create system/database backup.');
+        }
+
+        /*
+         *  Process the request to create a backup
+         */
+
+
+
+    }
+}

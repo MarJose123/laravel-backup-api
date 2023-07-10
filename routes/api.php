14af35f15a9bd20d00971e4945f810869d06ba78
Marjose <hypernate1@gmail.com>
2023-07-10 15:36:58 +0800

    added route and controller related

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,30 @@
 <?php
+use \Illuminate\Support\Facades\Route;
+use \MarJose123\LaravelBackupApi\Http\Controllers\BackupApiController;
+
+Route::middleware(config('backup-api.middleware'))
+    ->prefix(config('backup-api.route_prefix'))
+    ->name(config('backup-api.route_name_prefix'))
+    ->group(function (){
+        /*
+         * Creating a back-up
+         */
+        Route::post('/create', [BackupApiController::class, 'createBackup'])
+                ->name('create-backup');
+        /*
+         * Retrieve Back-up Disk Destination information
+         */
+        Route::get('/backup/disks', [BackupApiController::class, 'diskIndex'])
+            ->name('disks.index');
+        /*
+         * Retrieve Back-up information
+         */
+        Route::get('/backup', [BackupApiController::class, 'backupIndex'])
+            ->name('index');
+        /*
+         * Download the backup data
+         */
+        Route::get('/download/{id}', [BackupApiController::class, 'download'])
+            ->name('download');
+
+});

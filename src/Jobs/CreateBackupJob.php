f883931d0a586c3d5cd0b8f9733af2d8667920a2
Marjose <hypernate1@gmail.com>
2023-07-10 14:24:02 +0800

    created a Job class

---
+++ src/Jobs/CreateBackupJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MarJose123\LaravelBackupApi\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\Backup\Tasks\Backup\BackupJobFactory;
+
+class CreateBackupJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected string $option;
+
+    public function __construct()
+    {
+         $this->option = config('backup-api.options');
+    }
+
+    public function handle(): void
+    {
+        $backupJob = BackupJobFactory::createFromArray(config('backup'));
+
+        if($this->option === 'db') $backupJob->dontBackupFilesystem();
+        if($this->option === 'system') $backupJob->dontBackupDatabases();
+
+        $backupJob->setFilename($this->option.'-'.date('Y-m-d-H-i-s').'.zip');
+
+        $backupJob->run();
+
+    }
+}
